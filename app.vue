891327510d9abd8559c1f17c7a894e102dd9d050
iolite312 <leonwatertor18@gmail.com>
2024-06-22 20:06:09 +0200

    PoC of showing image and queue

--- app.vue
+++ app.vue
@@ -1,14 +1,20 @@
 <template>
-	<!-- <script defer src="https://sdk.scdn.co/spotify-player.js"></script> -->
 	<div>
 		<button @click="() => player.togglePlay()">Toggle play</button>
 		<button @click="() => player.previousTrack()">Previous</button>
 		<button @click="() => player.nextTrack()">Next</button>
+		<button @click="onStateChange()">Update State</button>
+		<div>
+			<p>Current: {{ track_window?.current_track?.name || 'No track playing' }}</p>
+			<p>Previous: {{ track_window?.previous_tracks[track_window?.previous_tracks.length - 1]?.name || 'No track playing' }}</p>
+			<img :src="track_window?.current_track?.album?.images[0]?.url" alt="Image of album" :height="track_window?.current_track?.album?.images[0]?.height || 0" :width="track_window?.current_track?.album?.images[0]?.width || 0">
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-	import { type SpotifyPlayer } from './types';
+	import { type Spotify, type SpotifyPlayer } from './types';
+
 	useHead({
 		title: 'Andromeda - Home',
 	})
@@ -17,6 +23,11 @@
 
 	let player : SpotifyPlayer;
 
+	
+	let playerState = ref<Spotify.PlaybackState>()
+
+	let track_window = ref<Spotify.PlaybackTrackWindow | undefined>(playerState.value?.track_window)
+	
 	CreateSpotify(config.public.spotifySecret)
 		.then((spotPlayer) => {
 			player = spotPlayer
@@ -24,6 +35,19 @@
 		.catch((err) => {
 			console.error(err)			
 		})
+	function onStateChange() {
+		player.addListener('player_state_changed', (state) => {
+			playerState.value = state
+			track_window.value = playerState.value?.track_window
+			console.log(track_window.value)
+		})
+	}
+	
+	onMounted(() => {
+		setTimeout(() => {
+			onStateChange()
+		}, 1000);
+	})
 </script>
 
 <style></style>
