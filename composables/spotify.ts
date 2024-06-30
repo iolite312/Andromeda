773a0637907fe6335ca59a292e46c3ba3225b0e1
iolite312 <leonwatertor18@gmail.com>
2024-06-30 16:35:13 +0200

    Automatic selecction of this device

--- composables/spotify.ts
+++ composables/spotify.ts
@@ -10,8 +10,24 @@ export const CreateSpotify = (token: string, name: string): Promise<SpotifyPlaye
             });
         
             // Ready
-            player.addListener('ready', ({ device_id }) => {
+            player.addListener('ready', async ({ device_id }) => {
                 console.log('Ready with Device ID', device_id);
+
+                const data = await $fetch('https://api.spotify.com/v1/me/player', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                    body: {
+                        device_ids: [device_id],
+                    }
+                });
+        
+                if (data == '') {
+                    throw new Error('No player found');
+                }
+                
                 resolve(player);
             });
         
@@ -42,17 +58,22 @@ export const CreateSpotify = (token: string, name: string): Promise<SpotifyPlaye
         }
     })
 }
-export const GetNewToken = async (refreshToken: string): Promise<string> => {
-    const result = await fetch("https://accounts.spotify.com/api/token", {
+export const GetNewToken = async (refreshToken: string, client_id: string): Promise<Spotify.Token> => {
+    const { data } = await useFetch<Spotify.Token>("https://accounts.spotify.com/api/token", {
         method: "POST",
         headers: {
             "Content-Type": "application/x-www-form-urlencoded"
         },
         body: new URLSearchParams({
+            "client_id": client_id,
             "grant_type": "refresh_token",
             "refresh_token": refreshToken
         })
     });
-    const json : Spotify.Token = await result.json();
-    return json.access_token;
+
+    if (data.value == null) {
+        throw new Error("No token found");
+    }
+
+    return data.value;
 }
