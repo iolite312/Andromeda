e04e90964dd9c20db8189ac40a561fd923fa8c25
iolite312 <leonwatertor18@gmail.com>
2024-07-08 17:39:14 +0200

    a small part of the spotify types

--- types/SpotifyApi.ts
+++ types/SpotifyApi.ts
@@ -1,3 +1,137 @@
 export declare namespace SpotifyApi {
+    interface Track {
+        album: Album
+        artists: Artist[]
+        available_markets: string[]
+        disc_number: number
+        duration_ms: number
+        explicit: boolean
+        external_ids: {
+            isrc: string
+        }
+        external_urls: {
+            spotify: string
+        }
+        href: string
+        id: string
+        name: string
+        popularity: number
+        preview_url: string
+        track_number: number
+        type: "track"
+        uri: string
+        is_local: boolean
+    }
+
+    interface Album {
+        album_type: string
+        total_tracks: number
+        available_markets: string[]
+        external_urls: {
+            spotify: string
+        }
+        href: string
+        id: string
+        images: Image[]
+        name: string
+        release_date: string
+        release_date_precision: string
+        type: "album"
+        uri: string
+        artists: Artist[]
+        tracks: Track[]
+        copyrights: string[]
+        external_ids: {
+            upc: string
+        }
+        genres: string[]
+        label: string
+        popularity: number
+    }
     
+    interface Image {
+        url: string
+        height: number
+        width: number
+    }
+
+    interface Artist {
+        external_urls: {
+            spotify: string
+        }
+        followers: {
+            href: string
+            total: number
+        }
+        genres: string[]
+        href: string
+        id: string
+        images: Image[]
+        name: string
+        popularity: number
+        type: "artist"
+        uri: string
+    }
+
+    interface Playlist {
+        collaborative: boolean
+        description: string
+        external_urls: {
+            spotify: string
+        }
+        followers: {
+            href: string
+            total: number
+        }
+        href: string
+        id: string
+        images: Image[]
+        name: string
+        owner: {
+            external_urls: {
+                spotify: string
+            }
+            followers: {
+                href: string
+                total: number
+            }
+            href: string
+            id: string
+            type: "user"
+            uri: string
+            display_name: string
+        }
+        public: boolean
+        snapshot_id: string
+        tracks: {
+            href: string
+            limit: number
+            next: string
+            offset: number
+            previous: string
+            total: number
+            items: PlaylistItem[]
+        }
+        type: "playlist"
+        uri: string
+    }
+
+    interface PlaylistItem {
+        added_at: string
+        added_by: {
+            external_urls: {
+                spotify: string
+            }
+            followers: {
+                href: string
+                total: number
+            }
+            href: string
+            id: string
+            type: "user"
+            uri: string
+        }
+        is_local: boolean
+        track: Track
+    }
 }
