870b1e076b3082a05501210759a707f50aa65757
iolite312 <leonwatertor18@gmail.com>
2024-07-27 22:25:16 +0200

    Revert "Temporary thing so i still can listen music while styling"
    This reverts commit 55b2ecbf5f92e755807f5f316a55c8d9ac704cbb.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,11 @@
 import type { Config } from 'tailwindcss/types'
+import colors from 'tailwindcss/colors'
 
 export default <Partial<Config>>{
     theme: {
         extend: {
             colors: {
-                spotify: {
-                    primary: "#1ED760",
-                    fg: {
-                        primary: "#E0E0E0",
-                        secondary: "#898989",
-                        tertiary: "#323232",
-                        light: {
-                            primary: "#1A1A1A",
-                            secondary: "#6E6E6E"
-                        }
-                    },
-                    bg: {
-                        main: "#060606",
-                        primary: "#111111",
-                        secondary: "#202020",
-                        light: {
-                            primary: "#F0F0F0",
-                        }
-                    }
-                }
+                primary: colors.green
             }
         }
     }
