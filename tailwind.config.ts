6efdf947455f61f8b0415a1b0c28b5dd53283600
iolite312 <leonwatertor18@gmail.com>
2024-07-27 22:27:07 +0200

    Colors from the design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,26 @@ export default <Partial<Config>>{
     theme: {
         extend: {
             colors: {
-                primary: colors.green
+                spotify: {
+                    primary: "#1ED760",
+                    fg: {
+                        primary: "#E0E0E0",
+                        secondary: "#898989",
+                        tertiary: "#323232",
+                        light: {
+                            primary: "#1A1A1A",
+                            secondary: "#6E6E6E"
+                        }
+                    },
+                    bg: {
+                        main: "#060606",
+                        primary: "#111111",
+                        secondary: "#202020",
+                        light: {
+                            primary: "#F0F0F0",
+                        }
+                    }
+                }
             }
         }
     }
