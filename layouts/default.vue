5921ed4ac1703745b7e9e6fc3ae2e0bee445e107
iolite312 <leonwatertor18@gmail.com>
2024-08-02 17:07:05 +0200

    default layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex bg-spotify-bg-main w-screen h-screen text-spotify-fg-secondary font-satoshi text-xl">
+        <Sidebar />
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
