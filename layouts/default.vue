6d715ac2a0adf9a8d53dec2ca201f23c66e699ea
iolite312 <leonwatertor18@gmail.com>
2024-08-03 23:01:13 +0200

    Fixed a layout issue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="bg-spotify-bg-main w-screen h-screen text-spotify-fg-secondary font-satoshi text-xl p-2">
-        <Navbar />
-        <div class="flex">
+    <div class="grid grid-cols-2 grid-rows-[auto_minmax(0,_1fr)] bg-spotify-bg-main w-screen h-screen text-spotify-fg-secondary font-satoshi text-xl p-2">
+        <Navbar class="col-span-2"/>
+        <div class="flex col-span-2">
             <Sidebar />
             <div class="ml-2">
                 <slot />
