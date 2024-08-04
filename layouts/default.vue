9ad2c55b9ffed46bb79265c62ad662eefed5edd3
iolite312 <leonwatertor18@gmail.com>
2024-08-04 18:54:01 +0200

    switched to a grid

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="grid grid-cols-2 grid-rows-[auto_minmax(0,_1fr)] bg-spotify-bg-main w-screen h-screen text-spotify-fg-secondary font-satoshi text-xl p-2">
+    <section class="grid grid-cols-2 grid-rows-[auto_minmax(0,_1fr)] bg-spotify-bg-main w-screen h-screen text-spotify-fg-secondary font-satoshi text-xl p-2">
         <Navbar class="col-span-2"/>
-        <div class="flex col-span-2">
+        <section class="flex col-span-2">
             <Sidebar />
-            <div class="ml-2 mt-2 w-full border rounded-xl border-spotify-bg-secondary p-6 overflow-auto">
-                <slot />
-            </div>
-        </div>
-    </div>
+            <section class="w-full ml-2 mt-2 flex flex-col">
+                <div class=" w-full border rounded-xl mb-1 border-spotify-bg-secondary p-6 overflow-auto grow">
+                    <slot />
+                </div>
+                <Musicbar />
+            </section>
+        </section>
+    </section>
 </template>
 
 <script setup lang="ts">
