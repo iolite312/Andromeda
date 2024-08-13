ce5dfdf32a25819f5c7f18e5e0b2a9a60a4701f4
iolite312 <leonwatertor18@gmail.com>
2024-08-14 00:43:46 +0200

    the library page

---
+++ pages/my-library.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="flex flex-row flex-wrap gap-x-3 gap-y-5">
+        <div  v-for="item in playlists?.items">
+            <PlaylistPreview :playlist="item" />
+        </div>
+        <div v-for="item in albums?.items">
+            <AlbumPreview :album="item.album" />
+        </div>
+        <div v-for="item in artists?.artists.items">
+            <ArtistPreview :artist="item" />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import type { SpotifyApi } from '~/types';
+    const { data: playlists } = useFetch<SpotifyApi.SpotifyApi.PlaylistRequest>("https://api.spotify.com/v1/me/playlists" , {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${useAuthStore().accessToken}`
+        }
+    })
+    const { data: albums } = useFetch<SpotifyApi.SpotifyApi.AlbumRequest>("https://api.spotify.com/v1/me/albums" , {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${useAuthStore().accessToken}`
+        }
+    })
+    const { data: artists } = useFetch<SpotifyApi.SpotifyApi.ArtistRequest>("https://api.spotify.com/v1/me/following?type=artist" , {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${useAuthStore().accessToken}`
+        }
+    })
+    console.log(artists.value)
+</script>
+
+<style scoped>
+
+</style>
